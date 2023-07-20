303e8686d6eefac4c328f276ecdbdcbe5dedaf75
Moritz Jörg <moke@mailbox.org>
2023-07-21 00:38:54 +0200

    Add ImageViewer and Button components, update dependencies
    The app's UI has been updated to use a new ImageViewer component and a reusable Button component. ImageViewer separates image styling from application layout, improving code readability. The Button component is configured to handle various themes for more flexible usage.
    
    Also, dependencies "@expo/vector-icons" and "expo-status-bar" were added to enhance user interface, and versions of existing dependencies are updated for optimal performance and latest features.

--- src/App.fs
+++ src/App.fs
@@ -1,11 +1,106 @@
 module App
 
 open Feliz
+open Fable.Core
 open Fable.Core.JsInterop
 open Feliz.ReactNative
 
+open Browser
+
+[<Import("FontAwesome", from="@expo/vector-icons/FontAwesome")>]
+let fontAwesome : ReactElement = jsNative
+
+[<Import("StatusBar", from="expo-status-bar")>]
+let expoStatusBar : ReactElement = jsNative
+
 let placeholderImage: IImageSource = importDefault "../assets/images/background-image.png"
 
+[<ReactComponent>]
+let ImageViewer (imageSource: IImageSource) =
+    Comp.image [
+        prop.style [
+            style.width 320
+            style.height 440
+            style.borderRadius 18
+        ]
+        prop.source imageSource
+    ]
+    
+[<ReactComponent>]
+let Button (label: string) (theme: string) =
+    if theme = "primary" then
+        Comp.view [
+            prop.style [
+               style.width 320
+               style.height 68
+               style.marginHorizontal 20
+               style.alignItems.center
+               style.justifyContent.center
+               style.padding 3
+               style.borderWidth 4
+               style.borderColor "#ffd33d"
+               style.borderRadius 18
+            ]
+            prop.children [
+                Comp.pressable [
+                    prop.style [
+                        style.borderRadius 10
+                        style.width 312
+                        style.height 60
+                        style.alignItems.center
+                        style.justifyContent.center
+                        style.flexDirection.row
+                        style.backgroundColor "#fff"
+                    ]
+                    prop.onPress (fun _ -> console.log("You pressed a button."))
+                    prop.children [
+                        Comp.text [
+                            prop.style [
+                                style.color "#25292e"
+                                style.fontSize 16.
+                            ]
+                            prop.text label
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    else
+        Comp.view [
+            prop.style [
+               style.width 320
+               style.height 68
+               style.marginHorizontal 20
+               style.alignItems.center
+               style.justifyContent.center
+               style.padding 3
+            ]
+            prop.children [
+                Comp.pressable [
+                    prop.style [
+                        style.borderRadius 10
+                        style.width 312
+                        style.height 60
+                        style.width 100.
+                        style.height 100.
+                        style.alignItems.center
+                        style.justifyContent.center
+                        style.flexDirection.row
+                    ]
+                    prop.onPress (fun _ -> console.log("You pressed a button."))
+                    prop.children [
+                        Comp.text [
+                            prop.style [
+                                style.color.white
+                                style.fontSize 16.
+                            ]
+                            prop.text label
+                        ]
+                    ]
+                ]
+            ]
+        ]
+        
 [<ReactComponent>]
 let App() =
     Comp.view [
@@ -22,24 +117,20 @@ let App() =
                     style.paddingTop 58
                 ]
                 prop.children [
-                    Comp.image [
-                        prop.style [
-                            style.width 320
-                            style.height 440
-                            style.borderRadius 18
-                        ]
-                        prop.source placeholderImage
-                    ]
+                    ImageViewer placeholderImage
                 ]
             ]
-            Comp.text [
+            Comp.view [
                 prop.style [
-                    style.color.white
+                    style.flex 0.33
+                    style.alignItems.center
                 ]
-                prop.text "Hello World!"
+                prop.children [
+                    
+                ]
             ]
-            Comp.statusBar [
-                prop.barStyle.default'
-            ]
+            Button "Choose a photo" "primary"
+            Button "Use this photo" "secondary"
+            expoStatusBar
         ]
     ]
